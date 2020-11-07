c5980bd8db8b23be1ac402789d84b3e425d942b4
Onee <onee.me@outlook.com>
2020-11-07 22:13:06 +0800

    add code style

--- pretty-git-diff/main.js
+++ pretty-git-diff/main.js
@@ -32,7 +32,22 @@ async function getCommmits() {
     const dir = process.argv[2];
     process.chdir(dir);
     const commits = await getCommmits();
-    const doc = new Document();
+    const doc = new Document({
+        styles: {
+            paragraphStyles: [
+                {
+                    id: "Code",
+                    name: "程序",
+                    basedOn: "Normal",
+                    next: "Code",
+                    quickFormat: true,
+                    run: {
+                        size: 16,
+                    },
+                },
+            ],
+        }
+    });
     const diffAndMessages = await Promise.all(commits.map(async (commit) => {
         const diffString = await execShellCommand(`git show ${commit.sha1}`);
         const diff = parser.parse(diffString);
@@ -57,34 +72,52 @@ async function getCommmits() {
                     const content = change.content;
                     const space = (content.match(/^\s+/) || [''])[0];
                     const noSpace = trim(content);
-                    const spaceText = new TextRun(space);
+                    const spaceText = new TextRun({
+                        text: space,
+                        style: 'Code',
+                    });
                     const noSpaceText = new TextRun({
                         text: noSpace,
                         bold: change.type === 'insert',
                         strike: change.type === 'delete',
+                        style: 'Code',
                     });
                     const paragraph = new Paragraph({
                         children: [spaceText, noSpaceText],
+                        style: 'Code',
                     });
                     return paragraph;
                 });
                 if (index < hunks.length - 1) {
                     // 如果不是最后一个 hunk，就加一个 ...
-                    changes.push(new Paragraph('...'));
+                    changes.push(new Paragraph({
+                        text: '...',
+                        style: 'Code',
+                    }));
                 }
                 return changes;
             });
             return [
                 new Paragraph({
                     text: `// ${diff.newPath}`,
+                    style: 'Code',
                 }),
                 // 如果没有从开头开始的，加个 ...
-                (first(diff.hunks).oldStart === 1 || first(diff.hunks).oldStart === 1) ? undefined : new Paragraph('...'),
+                (first(diff.hunks).oldStart === 1 || first(diff.hunks).oldStart === 1) ? undefined : new Paragraph({
+                    text: '...',
+                    style: 'Code',
+                }),
                 ...flatten(hunks),
                 // 简单的判断，如果以空行开头，说明不是最后一行，下方加个 ...
-                last(last(diff.hunks).changes).content.startsWith(' ') ? undefined : new Paragraph('...'),
+                last(last(diff.hunks).changes).content.startsWith(' ') ? undefined : new Paragraph({
+                    text: '...',
+                    style: 'Code',
+                }),
                 // 空行
-                new Paragraph(''),
+                new Paragraph({
+                    text: '',
+                    style: 'Code',
+                }),
             ]
         })));
         doc.addSection({
